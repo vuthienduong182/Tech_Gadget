0990c22240197839762c2e2b6635d9126872efdd
nguye <nguyenminhquan20403@gmail.com>
2024-01-16 20:34:57 +0700

    quan8:34

--- js/cart2.js
+++ js/cart2.js
@@ -5,21 +5,24 @@ let filterarray = [];
 let galleryarray =[
     {
         id : 1,
-        name: "Apple Watch Series 3",
-        src : "./image/IphoneXR.png",
+        name: "Iphone 12 Pro Max White",
+        price: "$900,00",
+        src : "./image/Iphone12ProMaxWhite.webp",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     
     },
     {
         id : 2,
-        name: "Iphone XS Max",
-        src : "./image/Iphone12Promax.png",
+        name: "Iphone 12 Pro Max Dark Blue",
+        price: "$900,00",
+        src : "./image/Iphone12ProMaxDarkBlue.webp",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     
     },
     {
         id : 3,
         name: "Iphone 13",
+        price: "$900,00",
         src : "./image/Iphone12.png",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     
@@ -27,53 +30,61 @@ let galleryarray =[
     {
         id : 4,
         name: "AirPod Pro",
+        price: "$900,00",
         src : "./image/AirPodPro.png",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     
     },
     {
         id : 5,
-        name: "AirPod 2",
-        src : "./image/AirPod2.png",
+        name: "AirPod 2nd Generation",
+        price: "$900,00",
+        src : "./image/Airpod2.webp",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
 
     {
         id : 6,
         name: "Headphone",
+        price: "$900,00",
         src : "./image/tainghe.png",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         id : 7,
         name: "MacBook",
+        price: "$900,00",
         src : "./image/macBook.png",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         id : 8,
         name: "Speaker",
+        price: "$900,00",
         src : "./image/loaJBL.png",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
 
     {
         id : 9,
-        name: "Samsung Galaxy S9+",
-        src : "./image/samsungGalaxys9.png",
+        name: "Samsung Galaxy Z Fold 5",
+        price: "$900,00",
+        src : "./image/galaxyzfold5xam1.webp",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
 
     {
         id : 10,
-        name: "AirPod MAX",
-        src : "./image/AirPodMax.png",
+        name: "Samsung Z Flip 5",
+        price: "$900,00",
+        src : "./image/Zplip5.webp",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
 
     {
         id : 11,
         name: "Xiaomi Mi11 Ultra",
+        price: "$900,00",
         src : "./image/XiaomiMi11Ultra.png",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
@@ -81,6 +92,7 @@ let galleryarray =[
     {
         id : 12,
         name: "Samsung Galaxy S21 Ultra 5G",
+        price: "$900,00",
         src : "./image/Samsung Galaxy S21 Ultra 5G.png",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },    
@@ -88,6 +100,21 @@ let galleryarray =[
     {
         id : 13,
         name: "MacBook Pro",
+        price: "$900,00",
+        src : "./image/MacBookPro.png",
+        desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        id : 14,
+        name: "MacBook Pro",
+        price: "$900,00",
+        src : "./image/MacBookPro.png",
+        desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        id : 13,
+        name: "MacBook Pro",
+        price: "$900,00",
         src : "./image/MacBookPro.png",
         desc :"Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
@@ -132,6 +159,7 @@ function showGallery(currarray) {
                     <h4 class="text-capitalize text-center">${currarray[i].name}</h4>
                     <img src="${currarray[i].src}" width="auto" height="auto">
                     <p class="mt-2">${currarray[i].desc}</p>
+                    <p class="mt-2">${currarray[i].price}</p>
                     <div class="d-flex justify-content-between">
                         <button class="btn btn-primary">More</button>
                         <button class="btn btn-primary">Add to cart</button>
